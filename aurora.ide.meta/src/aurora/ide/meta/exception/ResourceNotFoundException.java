17489c35eb49a390ff6956e4212dd1b15c39db5d
rufus.sly@gmail.com <shiliyan@users.noreply.github.com>
2012-02-16 06:25:35 +0000

    ide

---
+++ aurora.ide.meta/src/aurora/ide/meta/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package aurora.ide.meta.exception;
+
+public class ResourceNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9195281947764877182L;
+
+}
---
+++ aurora.ide.meta/src/aurora/ide/meta/project/AuroraMetaProject.java
@@ -0,0 +1,72 @@
+package aurora.ide.meta.project;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.QualifiedName;
+
+import aurora.ide.AuroraProjectNature;
+import aurora.ide.meta.exception.ResourceNotFoundException;
+
+public class AuroraMetaProject {
+
+	private IProject project;
+
+	public AuroraMetaProject(IProject project) {
+		super();
+		this.project = project;
+	}
+
+	public IProject getProject() {
+		return project;
+	}
+
+	public void setProject(IProject project) {
+		this.project = project;
+	}
+
+	public IFolder getScreenFolder() throws ResourceNotFoundException {
+		return this.getFolder(MetaProjectPropertyPage.SCREEN_QN);
+	}
+
+	public IFolder getModelFolder() throws ResourceNotFoundException {
+		return this.getFolder(MetaProjectPropertyPage.MODEL_QN);
+	}
+
+	public IFolder getTemplateFolder() throws ResourceNotFoundException {
+		return this.getFolder(MetaProjectPropertyPage.TEMPLATE_QN);
+	}
+
+	public IProject getAuroraProject() throws ResourceNotFoundException {
+
+		String name = getPersistentProperty(MetaProjectPropertyPage.AURORA_PROJECT_QN);
+		IProject p = project.getWorkspace().getRoot().getProject(name);
+		try {
+			if (p.exists() && AuroraProjectNature.hasAuroraNature(p)) {
+				return p;
+			}
+		} catch (CoreException e) {
+		}
+		throw new ResourceNotFoundException();
+	}
+
+	public IFolder getFolder(QualifiedName key)
+			throws ResourceNotFoundException {
+		String name = getPersistentProperty(key);
+		IFolder folder = project.getFolder(name);
+		if (folder.exists()) {
+			return folder;
+		}
+		throw new ResourceNotFoundException();
+	}
+
+	private String getPersistentProperty(QualifiedName key)
+			throws ResourceNotFoundException {
+		try {
+			return this.getProject().getPersistentProperty(key);
+		} catch (CoreException e) {
+			throw new ResourceNotFoundException();
+		}
+	}
+
+}
