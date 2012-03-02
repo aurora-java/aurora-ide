41c13ce39baf9c2f41ff83c75edaa9db23ef2455
wxjivm <wxjivm@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-03-02 07:17:48 +0000

    增加选择model对话框文件名过滤

--- aurora.ide.meta/src/aurora/ide/meta/gef/editors/wizard/dialog/SelectModelDialog.java
+++ aurora.ide.meta/src/aurora/ide/meta/gef/editors/wizard/dialog/SelectModelDialog.java
@@ -1,32 +1,47 @@
 package aurora.ide.meta.gef.editors.wizard.dialog;
 
+import java.util.regex.Pattern;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.ui.model.WorkbenchContentProvider;
 import org.eclipse.ui.model.WorkbenchLabelProvider;
+
+import aurora.ide.meta.MetaPlugin;
 
 public class SelectModelDialog extends Dialog {
 
 	private IResource resource;
 	private Object result;
-	private String message;
+	private ModelFilter filter;
 
-	public SelectModelDialog(Shell parentShell, IResource resource, String message) {
+	public SelectModelDialog(Shell parentShell, IResource resource) {
 		super(parentShell);
 		this.resource = resource;
-		this.message = message;
+		filter = new ModelFilter("");
 	}
 
 	@Override
@@ -44,14 +59,42 @@ public class SelectModelDialog extends Dialog {
 		Composite container = (Composite) super.createDialogArea(parent);
 		container.getShell().setText("文件选择");
 
-		Label label = new Label(container, SWT.None);
-		label.setText(message);
+		Composite composite = new Composite(container, SWT.None);
+		composite.setLayout(new GridLayout(2, false));
+		composite.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-		TreeViewer tree = new TreeViewer(container);
+		Label lblFileName = new Label(composite, SWT.None);
+		lblFileName.setText("文件名：");
+		final Text txtFileName = new Text(composite, SWT.BORDER);
+		txtFileName.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		Composite treeViewer = new Composite(composite, SWT.BORDER);
+		GridData gd = new GridData(GridData.FILL_BOTH);
+		gd.horizontalSpan = 2;
+		GridLayout gl = new GridLayout();
+		gl.marginWidth = 0;
+		gl.marginHeight = 0;
+		treeViewer.setLayout(gl);
+		treeViewer.setLayoutData(gd);
+		treeViewer.setBackground(getShell().getDisplay().getSystemColor(SWT.COLOR_WHITE));
+
+		final ToolBar toolBar = new ToolBar(treeViewer, SWT.FLAT);
+		toolBar.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		toolBar.addPaintListener(new PaintListener() {
+			@Override
+			public void paintControl(PaintEvent e) {
+				e.gc.setForeground(getShell().getDisplay().getSystemColor(SWT.COLOR_GRAY));
+				e.gc.drawLine(0, e.height - 1, e.width, e.height - 1);
+			}
+		});
+
+		final TreeViewer tree = new TreeViewer(treeViewer, SWT.None);
 		tree.getTree().setLayoutData(new GridData(GridData.FILL_BOTH));
 		tree.setContentProvider(new WorkbenchContentProvider());
 		tree.setLabelProvider(new WorkbenchLabelProvider());
 		tree.setInput(resource);
+		tree.addFilter(filter);
+
 		tree.addSelectionChangedListener(new ISelectionChangedListener() {
 			public void selectionChanged(SelectionChangedEvent event) {
 				TreeSelection ts = (TreeSelection) event.getSelection();
@@ -63,6 +106,37 @@ public class SelectModelDialog extends Dialog {
 				}
 			}
 		});
+
+		txtFileName.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				tree.removeFilter(filter);
+				filter = new ModelFilter(txtFileName.getText());
+				tree.addFilter(filter);
+			}
+		});
+
+		ToolBarManager toolBarManager = new ToolBarManager(toolBar);
+		Action expand = new Action("expand", MetaPlugin.imageDescriptorFromPlugin(MetaPlugin.PLUGIN_ID, "icons/expandall.gif")) {
+			public void run() {
+				tree.getControl().setRedraw(false);
+				tree.expandAll();
+				tree.getControl().setRedraw(true);
+			}
+		};
+
+		Action collapse = new Action("collapse", MetaPlugin.imageDescriptorFromPlugin(MetaPlugin.PLUGIN_ID, "icons/collapseall.gif")) {
+			public void run() {
+				tree.getControl().setRedraw(false);
+				tree.collapseAll();
+				tree.getControl().setRedraw(true);
+			}
+		};
+		toolBarManager.add(expand);
+		toolBarManager.add(collapse);
+		toolBarManager.update(true);
+		toolBar.pack();
+
 		return container;
 	}
 
@@ -79,4 +153,33 @@ public class SelectModelDialog extends Dialog {
 		getButton(OK).setEnabled(false);
 		return control;
 	}
+}
+
+class ModelFilter extends ViewerFilter {
+	private String fileName;
+
+	public ModelFilter(String fileName) {
+		fileName = fileName.replaceAll("\\\\", "\\\\\\\\");
+		fileName = fileName.replaceAll("\\.", "\\\\.");
+		fileName = fileName.replaceAll("\\?", ".");
+		fileName = fileName.replaceAll("\\*", ".*");
+		fileName += ".*";
+		this.fileName = fileName;
+	}
+
+	@Override
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		if (element instanceof IFile) {
+			IFile o = (IFile) element;
+			if (fileName.length() == 0) {
+				return true;
+			} else if (Pattern.matches("^" + fileName, o.getName())) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
