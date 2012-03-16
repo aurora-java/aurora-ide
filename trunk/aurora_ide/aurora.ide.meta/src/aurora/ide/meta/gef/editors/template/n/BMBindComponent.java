fab4a7469cc6fc3f98dc398f3c49acaef2cebe78
rufus.sly@gmail.com <rufus.sly@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-03-16 09:25:24 +0000

    ide:new ui template

---
+++ trunk/aurora_ide/aurora.ide.meta/src/aurora/ide/meta/gef/editors/template/n/BMBindComponent.java
@@ -0,0 +1,22 @@
+package aurora.ide.meta.gef.editors.template.n;
+
+public class BMBindComponent extends Component {
+	private String queryComponent;
+	private String bmReferenceID;
+
+	public String getBmReferenceID() {
+		return bmReferenceID;
+	}
+
+	public void setBmReferenceID(String bmReferenceID) {
+		this.bmReferenceID = bmReferenceID;
+	}
+
+	public String getQueryComponent() {
+		return queryComponent;
+	}
+
+	public void setQueryComponent(String queryComponent) {
+		this.queryComponent = queryComponent;
+	}
+}
