5264e8797a7f5103105a72c885b14b41931c98f3
wxjivm <wxjivm@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-03-31 02:09:49 +0000

    添加display属性

--- aurora.ide.meta/src/aurora/ide/meta/gef/editors/models/ViewDiagram.java
+++ aurora.ide.meta/src/aurora/ide/meta/gef/editors/models/ViewDiagram.java
@@ -8,11 +8,12 @@ import org.eclipse.ui.views.properties.IPropertyDescriptor;
 public class ViewDiagram extends Container {
 	private static final long serialVersionUID = -9196440587781890208L;
 	public static final int DLabelWidth = 80;
-	private static Class<?>[] unsupported = { Toolbar.class, Navbar.class,
-			GridColumn.class, TabItem.class };
+	private static Class<?>[] unsupported = { Toolbar.class, Navbar.class, GridColumn.class, TabItem.class };
 
-//	private List<Link> links = new ArrayList<Link>();
+	// private List<Link> links = new ArrayList<Link>();
 	private String bindTemplate = "";
+
+	private boolean isForDisplay = false;
 
 	@Override
 	public boolean isResponsibleChild(AuroraComponent component) {
@@ -23,62 +24,63 @@ public class ViewDiagram extends Container {
 		return super.isResponsibleChild(component);
 	}
 
-//	public List<Link> getLinks() {
-//		return links;
-//	}
-//
-//	/**
-//	 */
-//	public void addLink(Link link) {
-//		links.add(link);
-//	}
-//
-//	/**
-//	 */
-//	public void removeLink(Link link) {
-//		links.remove(link);
-//	}
+	// public List<Link> getLinks() {
+	// return links;
+	// }
+	//
+	// /**
+	// */
+	// public void addLink(Link link) {
+	// links.add(link);
+	// }
+	//
+	// /**
+	// */
+	// public void removeLink(Link link) {
+	// links.remove(link);
+	// }
 
 	@Override
 	public IPropertyDescriptor[] getPropertyDescriptors() {
-//		IPropertyDescriptor[] pds = new IPropertyDescriptor[links.size() + 1];
-//		for (int i = 0; i < links.size(); i++) {
-//			pds[i] = new LinkPropertyDescriptor(i, "Link");
-//		}
-//		pds[pds.length - 1] = new AddLinkPropertyDescriptor("add_link",
-//				"ADD Link");
-//		return pds;
+		// IPropertyDescriptor[] pds = new IPropertyDescriptor[links.size() +
+		// 1];
+		// for (int i = 0; i < links.size(); i++) {
+		// pds[i] = new LinkPropertyDescriptor(i, "Link");
+		// }
+		// pds[pds.length - 1] = new AddLinkPropertyDescriptor("add_link",
+		// "ADD Link");
+		// return pds;
 		return new IPropertyDescriptor[0];
 	}
 
 	@Override
 	public Object getPropertyValue(Object key) {
-//		if (key instanceof Integer) {
-//			return links.get(((Integer) key).intValue());
-//		}
-//		if ("add_link".equals(key))
-//			return null;
+		// if (key instanceof Integer) {
+		// return links.get(((Integer) key).intValue());
+		// }
+		// if ("add_link".equals(key))
+		// return null;
 		return super.getPropertyValue(key);
 	}
 
 	@Override
 	public void setPropertyValue(Object key, Object val) {
-//		if (key instanceof Integer && val instanceof Link) {
-//			if (val instanceof DeadLink) {
-//				links.remove(((Integer) key).intValue());
-//			} else {
-//				Link nVal = (Link) val;
-//				Link link = links.get(((Integer) key).intValue());
-//				link.setUrl(nVal.getUrl());
-//				link.setModel(nVal.getModel());
-//				link.setModelaction(nVal.getModelaction());
-//			}
-//			firePropertyChange("Link", null, val);
-//		}
-//		if ("add_link".equals(key)) {
-//			links.add((Link) val);
-//			firePropertyChange("Link", null, val);
-//		}
+		// if (key instanceof Integer && val instanceof Link) {
+		// if (val instanceof DeadLink) {
+		// links.remove(((Integer) key).intValue());
+		// } else {
+		// Link nVal = (Link) val;
+		// Link link = links.get(((Integer) key).intValue());
+		// link.setUrl(nVal.getUrl());
+		// link.setModel(nVal.getModel());
+		// link.setModelaction(nVal.getModelaction());
+		// }
+		// firePropertyChange("Link", null, val);
+		// }
+		// if ("add_link".equals(key)) {
+		// links.add((Link) val);
+		// firePropertyChange("Link", null, val);
+		// }
 		super.setPropertyValue(key, val);
 	}
 
@@ -104,8 +106,7 @@ public class ViewDiagram extends Container {
 		List<Container> containers = getContainers(this);
 		for (Container container : containers) {
 			String sectionType = container.getSectionType();
-			if (Container.SECTION_TYPE_QUERY.equals(sectionType)
-					|| Container.SECTION_TYPE_RESULT.equals(sectionType)) {
+			if (Container.SECTION_TYPE_QUERY.equals(sectionType) || Container.SECTION_TYPE_RESULT.equals(sectionType)) {
 				String model = container.getDataset().getModel();
 				if (null != model) {
 					models.add(model);
@@ -121,8 +122,7 @@ public class ViewDiagram extends Container {
 		for (AuroraComponent ac : children) {
 			if (ac instanceof Container) {
 				String sectionType = ((Container) ac).getSectionType();
-				if(Container.SECTION_TYPE_QUERY.equals(sectionType)
-						|| Container.SECTION_TYPE_RESULT.equals(sectionType)){
+				if (Container.SECTION_TYPE_QUERY.equals(sectionType) || Container.SECTION_TYPE_RESULT.equals(sectionType)) {
 					containers.add((Container) ac);
 				}
 				containers.addAll(getSectionContainers((Container) ac));
@@ -130,7 +130,7 @@ public class ViewDiagram extends Container {
 		}
 		return containers;
 	}
-	
+
 	public List<Container> getContainers(Container container) {
 		List<Container> containers = new ArrayList<Container>();
 		List<AuroraComponent> children = container.getChildren();
@@ -141,6 +141,14 @@ public class ViewDiagram extends Container {
 			}
 		}
 		return containers;
+	}
+
+	public boolean isForDisplay() {
+		return isForDisplay;
+	}
+
+	public void setForDisplay(boolean isForDisplay) {
+		this.isForDisplay = isForDisplay;
 	}
 
 }
