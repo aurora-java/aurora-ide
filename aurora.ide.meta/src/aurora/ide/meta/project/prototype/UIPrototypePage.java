c7ef96e923157b512e939cb849bfe888286e5e05
wxjivm <wxjivm@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-03-31 01:55:52 +0000

    添加首选项页模板管理

---
+++ aurora.ide.meta/src/aurora/ide/meta/project/prototype/UIPrototypePage.java
@@ -0,0 +1,34 @@
+package aurora.ide.meta.project.prototype;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import aurora.ide.meta.gef.editors.template.TComposite;
+import aurora.ide.meta.gef.editors.template.Template;
+import aurora.ide.meta.gef.editors.template.parse.TemplateHelper;
+
+public class UIPrototypePage extends PreferencePage implements IWorkbenchPreferencePage {
+
+	public void init(IWorkbench workbench) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		// TODO Auto-generated method stub
+		Composite container = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		container.setLayout(layout);
+
+		TComposite tComposite = new TComposite(container, SWT.BORDER, TemplateHelper.loadTemplate());
+
+		return container;
+	}
+}
