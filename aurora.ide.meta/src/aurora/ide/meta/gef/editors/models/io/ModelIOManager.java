1b6def7a87dd0a75b9138ba9a972c05111b81d86
jeschou.zsy@gmail.com <jeschou@users.noreply.github.com>
2012-04-11 04:53:19 +0000

    增加对initmodel  存取的支持

--- aurora.ide.meta/src/aurora/ide/meta/gef/editors/models/io/ModelIOManager.java
+++ aurora.ide.meta/src/aurora/ide/meta/gef/editors/models/io/ModelIOManager.java
@@ -2,9 +2,10 @@ package aurora.ide.meta.gef.editors.models.io;
 
 import java.util.List;
 
-import aurora.ide.api.composite.map.CommentCompositeMap;
 import uncertain.composite.CompositeMap;
+import aurora.ide.api.composite.map.CommentCompositeMap;
 import aurora.ide.meta.gef.editors.models.AuroraComponent;
+import aurora.ide.meta.gef.editors.models.InitModel;
 import aurora.ide.meta.gef.editors.models.ViewDiagram;
 
 public class ModelIOManager {
@@ -12,6 +13,8 @@ public class ModelIOManager {
 	public static final String NS_CHILDLIST = "cl";
 	public static final String NS_CHILDLIST_URI = "http://meta.editor/childlist";
 	public static final String BIND_TEMPLATE = "bindTemplate";
+	public static final String initmodel_node_name = InitModel.class
+			.getSimpleName() + "s";
 
 	ModelIOContext mic = new ModelIOContext();
 
@@ -26,6 +29,16 @@ public class ModelIOManager {
 		CompositeMap root = new CommentCompositeMap();
 		root.setName(diagram.getClass().getSimpleName());
 		root.put(BIND_TEMPLATE, diagram.getBindTemplate());
+		List<InitModel> imList = diagram.getInitModels();
+		if (imList.size() > 0) {
+			CompositeMap imMap = new CommentCompositeMap(initmodel_node_name);
+			InitModelHandler imhandler = new InitModelHandler();
+			for (InitModel im : imList) {
+				CompositeMap m = imhandler.toCompositeMap(im, mic);
+				imMap.addChild(m);
+			}
+			root.addChild(imMap);
+		}
 		for (AuroraComponent ac : diagram.getChildren()) {
 			IOHandler ioh = IOHandlerUtil.getHandler(ac);
 			root.addChild(ioh.toCompositeMap(ac, mic));
@@ -37,16 +50,30 @@ public class ModelIOManager {
 		if (!ViewDiagram.class.getSimpleName().equalsIgnoreCase(root.getName()))
 			return null;
 		ViewDiagram dia = new ViewDiagram();
+
 		String tpl = root.getString(BIND_TEMPLATE);
 		dia.setBindTemplate(tpl);
 		@SuppressWarnings("unchecked")
 		List<CompositeMap> list = root.getChildsNotNull();
 		for (CompositeMap map : list) {
+			if (map.getName().equals(initmodel_node_name)) {
+				restoreInitModels(dia, map);
+				continue;
+			}
 			IOHandler ioh = IOHandlerUtil.getHandler(map);
 			dia.addChild(ioh.fromCompositeMap(map, mic));
 		}
 		recoverReference(dia);
 		return dia;
+	}
+
+	private void restoreInitModels(ViewDiagram dia, CompositeMap imMap) {
+		List<CompositeMap> list = imMap.getChildsNotNull();
+		InitModelHandler imh = new InitModelHandler();
+		for (CompositeMap m : list) {
+			InitModel im = (InitModel) imh.fromCompositeMap(m, mic);
+			dia.addInitModels(im);
+		}
 	}
 
 	private void recoverReference(ViewDiagram dia) {
