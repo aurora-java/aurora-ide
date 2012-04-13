6ec4f27505fc1d5d809cd130b2bfa4f41b729ecc
wxjivm <wxjivm@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-04-13 07:10:23 +0000

    修改UIP向导页

--- aurora.ide.meta/src/aurora/ide/meta/gef/editors/wizard/dialog/TLabel.java
+++ aurora.ide.meta/src/aurora/ide/meta/gef/editors/wizard/dialog/TLabel.java
@@ -1,17 +1,20 @@
 package aurora.ide.meta.gef.editors.wizard.dialog;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.TypedListener;
 
 public class TLabel extends Composite {
 
@@ -28,38 +31,38 @@ public class TLabel extends Composite {
 		canvas = new Canvas(this, SWT.None);
 		canvas.setLayoutData(new GridData(GridData.FILL_BOTH));
 		canvas.setBackground(getShell().getDisplay().getSystemColor(SWT.COLOR_WHITE));
-		canvas.addMouseListener(new MouseListener() {
-			
-			public void mouseUp(MouseEvent e) {
-				// TODO Auto-generated method stub
-				System.out.println("up");
-			}
-			
+		canvas.addMouseListener(new MouseAdapter() {
 			public void mouseDown(MouseEvent e) {
-				// TODO Auto-generated method stub
-				System.out.println("down");
-			}
-			
-			public void mouseDoubleClick(MouseEvent e) {
-				// TODO Auto-generated method stub
-				System.out.println("double");
+				notifyListeners(SWT.MouseDown, new Event());
 			}
 		});
-
-		//label.setCursor(getShell().getDisplay().getSystemCursor(SWT.CURSOR_HAND));
 		
 		label = new Label(this, SWT.CENTER);
 		label.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-
+		label.addMouseListener(new MouseAdapter() {
+			public void mouseDown(MouseEvent e) {
+				notifyListeners(SWT.MouseDown, new Event());
+			}
+		});
+		
 		addPaintListener(new PaintListener() {
 			public void paintControl(PaintEvent e) {
 				onPaint(e);
 			}
 		});
-		
-		
+
 	}
 
+	public void addSelectionListener(SelectionListener listener) {
+		checkWidget();
+		if (listener == null) {
+			SWT.error(SWT.ERROR_NULL_ARGUMENT);
+		}
+		TypedListener typedListener = new TypedListener(listener);
+		addListener(SWT.Selection, typedListener);
+		addListener(SWT.DefaultSelection, typedListener);
+	}
+	
 	protected void onPaint(PaintEvent e) {
 		if (image != null) {
 			canvas.addPaintListener(new PaintListener() {
