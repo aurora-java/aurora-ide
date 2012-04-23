9c63b5a3a013ecebd64e786e1b9edac3aa555cb6
wxjivm <wxjivm@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-04-23 08:46:39 +0000

    relation的localfield替换

--- trunk/aurora_ide/aurora.ide.meta/src/aurora/ide/meta/gef/editors/template/handle/UpdateTemplateHandle.java
+++ trunk/aurora_ide/aurora.ide.meta/src/aurora/ide/meta/gef/editors/template/handle/UpdateTemplateHandle.java
@@ -1,29 +1,14 @@
 package aurora.ide.meta.gef.editors.template.handle;
 
 import uncertain.composite.CompositeMap;
-import aurora.ide.meta.gef.editors.models.AuroraComponent;
-import aurora.ide.meta.gef.editors.models.Container;
 import aurora.ide.meta.gef.editors.models.GridColumn;
 import aurora.ide.meta.gef.editors.models.ViewDiagram;
-import aurora.ide.meta.gef.editors.template.BMReference;
 
 public class UpdateTemplateHandle extends TemplateHandle {
 
 	@Override
 	public void fill(ViewDiagram viewDiagram) {
-		this.viewDiagram = viewDiagram;
-		for (BMReference bm : modelRelated.keySet()) {
-			for (Container ac : modelRelated.get(bm)) {
-				fillContainer(ac, bm);
-			}
-		}
-
-		for (BMReference bm : queryModelRelated.keySet()) {
-			AuroraComponent ac = auroraComponents.get(queryModelRelated.get(bm));
-			if (ac instanceof Container) {
-				fillQueryBox((Container) ac, bm);
-			}
-		}
+		super.fill(viewDiagram);
 	}
 	
 	@Override
