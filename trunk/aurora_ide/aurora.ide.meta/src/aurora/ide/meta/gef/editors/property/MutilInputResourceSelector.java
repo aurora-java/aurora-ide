3763087311c44ef86e3cd2d486ea82dbd6d2ed61
jeschou.zsy@gmail.com <jeschou.zsy@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-04-23 06:24:19 +0000

    调整ResourceContentProvider ,继承父类的内部类

--- trunk/aurora_ide/aurora.ide.meta/src/aurora/ide/meta/gef/editors/property/MutilInputResourceSelector.java
+++ trunk/aurora_ide/aurora.ide.meta/src/aurora/ide/meta/gef/editors/property/MutilInputResourceSelector.java
@@ -1,19 +1,11 @@
 package aurora.ide.meta.gef.editors.property;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-
 import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.model.WorkbenchContentProvider;
 
 public class MutilInputResourceSelector extends ResourceSelector {
 
@@ -49,8 +41,8 @@ public class MutilInputResourceSelector extends ResourceSelector {
 		this.getTreeViewer().setContentProvider(new ResourceContentProvider());
 	}
 
-	private class ResourceContentProvider extends WorkbenchContentProvider
-			implements Comparator<IResource> {
+	private class ResourceContentProvider extends
+			ResourceSelector.ResourceContentProvider {
 		public Object[] getElements(Object element) {
 			if (element instanceof Object[]) {
 				Object[] elements = (Object[]) element;
@@ -58,52 +50,5 @@ public class MutilInputResourceSelector extends ResourceSelector {
 			}
 			return getChildren(element);
 		}
-
-		public Object[] getChildren(Object element) {
-
-			Object[] objs = super.getChildren(element);
-			ArrayList<IResource> als = new ArrayList<IResource>();
-			for (Object o : objs) {
-				IResource r = (IResource) o;
-				if (r.getName().startsWith("."))
-					continue;
-				if (hasChildren(r))
-					als.add(r);
-				else if (accept(r.getName()))
-					als.add(r);
-			}
-			IResource[] res = new IResource[als.size()];
-			als.toArray(res);
-			Arrays.sort(res, this);
-			return res;
-		}
-
-		public boolean hasChildren(Object element) {
-			if (element instanceof IContainer) {
-				IContainer folder = (IContainer) element;
-				Object[] res = getChildren(folder);
-				if (res.length == 0)
-					return false;
-				for (Object o : res) {
-					if (o instanceof IFile) {
-						return true;
-					} else if ((o instanceof IFolder) && hasChildren(o)) {
-						return true;
-					}
-				}
-			}
-			return false;
-		}
-
-		public int compare(IResource o1, IResource o2) {
-			int i1 = (o1 instanceof IFolder) ? 0 : 1;
-			int i2 = (o2 instanceof IFolder) ? 0 : 1;
-			int r = i1 - i2;
-			if (r == 0)
-				r = o1.getName().toLowerCase()
-						.compareTo(o2.getName().toLowerCase());
-			return r;
-		}
 	}
-
 }
