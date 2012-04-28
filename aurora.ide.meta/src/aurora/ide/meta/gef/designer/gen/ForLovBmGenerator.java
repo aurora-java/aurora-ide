0c5ef371b65599ea9f65d3543c4bf6a0ce12456f
jeschou.zsy@gmail.com <jeschou@users.noreply.github.com>
2012-04-28 02:30:01 +0000

    修正潜在的问题

--- aurora.ide.meta/src/aurora/ide/meta/gef/designer/gen/ForLovBmGenerator.java
+++ aurora.ide.meta/src/aurora/ide/meta/gef/designer/gen/ForLovBmGenerator.java
@@ -1,27 +1,22 @@
 package aurora.ide.meta.gef.designer.gen;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 
 import uncertain.composite.CompositeMap;
+import aurora.ide.builder.ResourceUtil;
 import aurora.ide.meta.gef.designer.BMCompositeMap;
 import aurora.ide.meta.gef.designer.model.BMModel;
 import aurora.ide.search.cache.CacheManager;
-import aurora.ide.search.core.Util;
 
 public class ForLovBmGenerator extends AbstractBmGenerator {
 
-	private BMModel model;
 	private String baseBmPath;
-	private IProject auroraProject;
 	private IFile bmFile;
 
 	public ForLovBmGenerator(BMModel model, IFile baseBMFile) {
 		super();
-		this.model = model;
 		this.bmFile = baseBMFile;
-		baseBmPath = Util.toPKG(baseBMFile.getFullPath().removeFileExtension());
-		this.auroraProject = baseBMFile.getProject();
+		baseBmPath = ResourceUtil.getBmPkgPath(baseBMFile);
 	}
 
 	@Override
@@ -43,6 +38,8 @@ public class ForLovBmGenerator extends AbstractBmGenerator {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		if (bmMap == null)
+			return;
 		BMCompositeMap bcm = new BMCompositeMap(bmMap);
 		String[] fnames = { bcm.getPkFieldName(),
 				bcm.getDefaultDisplayFieldName() };
