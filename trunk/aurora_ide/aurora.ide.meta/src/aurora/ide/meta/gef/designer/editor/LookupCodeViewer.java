a3ac938cabc759c729093aad2cc88c7fe74f6ba1
jeschou.zsy@gmail.com <jeschou.zsy@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-06-05 05:57:57 +0000

    sys_code查看器，可以选择用，可以创建到数据库用

---
+++ trunk/aurora_ide/aurora.ide.meta/src/aurora/ide/meta/gef/designer/editor/LookupCodeViewer.java
@@ -0,0 +1,106 @@
+package aurora.ide.meta.gef.designer.editor;
+
+import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+
+import uncertain.composite.CompositeMap;
+
+public class LookupCodeViewer extends TreeViewer {
+	private Tree tree;
+
+	public LookupCodeViewer(Composite parent) {
+		super(parent);
+		init(parent);
+	}
+
+	public LookupCodeViewer(Composite parent, int style) {
+		super(parent, style);
+		init(parent);
+	}
+
+	private void init(Composite parent) {
+		tree = getTree();
+		setContentProvider(new TreeConentProvider());
+		setLabelProvider(new TreeLabelProvider());
+		//
+	}
+
+	public void select(String code) {
+		for (TreeItem ti : tree.getItems()) {
+			ti.getStyle();
+			if (LookupCodeUtil.getCode(ti.getData()).equalsIgnoreCase(code)) {
+				setSelection(new StructuredSelection(ti.getData()));
+				break;
+			}
+		}
+	}
+
+	class TreeConentProvider implements ITreeContentProvider {
+
+		public void dispose() {
+
+		}
+
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+
+		}
+
+		public Object[] getElements(Object inputElement) {
+			CompositeMap codemap = (CompositeMap) inputElement;
+			return codemap.getChildsNotNull().toArray();
+		}
+
+		public Object[] getChildren(Object parentElement) {
+			CompositeMap m = (CompositeMap) parentElement;
+			return m.getChildsNotNull().toArray();
+		}
+
+		public Object getParent(Object element) {
+			CompositeMap m = (CompositeMap) element;
+			return m.getParent();
+		}
+
+		public boolean hasChildren(Object element) {
+			if ((tree.getStyle() & SWT.CHECK) != 0)
+				return false;
+			return LookupCodeUtil.isCode(element);
+		}
+	}
+
+	class TreeLabelProvider extends LabelProvider implements IColorProvider {
+
+		@Override
+		public String getText(Object element) {
+			if (LookupCodeUtil.isCode(element)) {
+				String code = LookupCodeUtil.getCode(element);
+				if (((CompositeMap) element).getBoolean("exists"))
+					code += "    (exists)";
+				return code;
+			}
+			return LookupCodeUtil.getValueAsString(element);
+		}
+
+		public Color getForeground(Object element) {
+			String t = getText(element);
+			if (t.contains("<"))// <..>means error
+				return new Color(null, 255, 0, 0);
+			if (LookupCodeUtil.isValue(element)) {
+				return new Color(null, 128, 128, 128);
+			}
+			return null;
+		}
+
+		public Color getBackground(Object element) {
+			return null;
+		}
+	}
+}
