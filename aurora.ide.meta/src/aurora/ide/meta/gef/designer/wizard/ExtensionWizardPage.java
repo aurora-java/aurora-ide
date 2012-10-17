122f38480c9b401a9ad0561c703f69e4d13891ba
jeschou.zsy@gmail.com <jeschou@users.noreply.github.com>
2012-10-17 07:50:42 +0000

    修改配置扩展页面的设计

--- aurora.ide.meta/src/aurora/ide/meta/gef/designer/wizard/ExtensionWizardPage.java
+++ aurora.ide.meta/src/aurora/ide/meta/gef/designer/wizard/ExtensionWizardPage.java
@@ -6,25 +6,15 @@ import java.util.List;
 
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.StackLayout;
-import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.swt.events.MouseMoveListener;
-import org.eclipse.swt.events.MouseTrackListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Tree;
-import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
 
 import aurora.ide.meta.gef.designer.DesignerUtil;
 import aurora.ide.meta.gef.designer.IDesignerConst;
@@ -33,17 +23,14 @@ import aurora.ide.meta.gef.designer.model.BMModel;
 import aurora.ide.meta.gef.designer.model.Record;
 
 public class ExtensionWizardPage extends WizardPage implements
-		MouseTrackListener, MouseMoveListener, SelectionListener {
+		SelectionListener {
 
-	private Tree tree;
 	private String[] infos = { Messages.ExtensionWizardPage_1,
 			Messages.ExtensionWizardPage_0, Messages.ExtensionWizardPage_2 };
 	private String[] defaultSelection = {};
-	private Shell tip;
-	private Label label = null;
 	private BMModel model;
-	private Composite stackComposite;
-	private StackLayout layout;
+	private TabFolder tabFolder;
+	private Button[] checkBoxs;
 	private SimpleBMModelViewer[] viewers = new SimpleBMModelViewer[3];
 
 	/**
@@ -65,18 +52,19 @@ public class ExtensionWizardPage extends WizardPage implements
 		setControl(container);
 		container.setLayout(new GridLayout(2, false));
 		List<String> list = Arrays.asList(defaultSelection);
-		tree = new Tree(container, SWT.BORDER | SWT.CHECK | SWT.SINGLE);
-		for (int i = 0; i < IDesignerConst.AE_TYPES.length; i++) {
-			TreeItem ti = new TreeItem(tree, SWT.NONE);
-			ti.setText("generate _for_" + IDesignerConst.AE_TYPES[i] + ".bm"); //$NON-NLS-1$ //$NON-NLS-2$
-			ti.setData(IDesignerConst.AE_TYPES[i]);
-			ti.setChecked(list.indexOf(IDesignerConst.AE_TYPES[i]) != -1);
+		Composite com = new Composite(container, 0);
+		com.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 2));
+		com.setLayout(new GridLayout(1, true));
+		checkBoxs = new Button[IDesignerConst.AE_TYPES.length];
+		for (int i = 0; i < checkBoxs.length; i++) {
+			checkBoxs[i] = new Button(com, SWT.CHECK);
+			checkBoxs[i].setText("generate _for_" + IDesignerConst.AE_TYPES[i]
+					+ ".bm");
+			checkBoxs[i].setData(IDesignerConst.AE_TYPES[i]);
+			checkBoxs[i]
+					.setSelection(list.indexOf(IDesignerConst.AE_TYPES[i]) != -1);
+			checkBoxs[i].setToolTipText(infos[i]);
 		}
-		tree.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 2));
-		tree.setBackground(container.getBackground());
-		tree.addMouseMoveListener(this);
-		tree.addMouseTrackListener(this);
-		tree.addSelectionListener(this);
 		createTopRight(container);
 		createBottom(container);
 
@@ -89,8 +77,8 @@ public class ExtensionWizardPage extends WizardPage implements
 		btnSelectAll.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				for (TreeItem ti : tree.getItems())
-					ti.setChecked(true);
+				for (Button b : checkBoxs)
+					b.setSelection(true);
 			}
 		});
 		btnSelectAll.setText("Select All"); //$NON-NLS-1$
@@ -101,23 +89,26 @@ public class ExtensionWizardPage extends WizardPage implements
 		btnDesselectAll.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				for (TreeItem ti : tree.getItems())
-					ti.setChecked(false);
+				for (Button b : checkBoxs)
+					b.setSelection(false);
 			}
 		});
 		btnDesselectAll.setText("Deselect All"); //$NON-NLS-1$
 	}
 
 	private void createBottom(Composite container) {
-		stackComposite = new Composite(container, SWT.BORDER);
-		stackComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true,
-				true, 1, 1));
-		layout = new StackLayout();
-		stackComposite.setLayout(layout);
+		tabFolder = new TabFolder(container, 0);
+		tabFolder.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1,
+				1));
 		for (int i = 0; i < viewers.length; i++) {
-			viewers[i] = new SimpleBMModelViewer(stackComposite, 0);
-			viewers[i].setDisplayMode((String) tree.getItem(i).getData());
+			TabItem ti = new TabItem(tabFolder, 0);
+			viewers[i] = new SimpleBMModelViewer(tabFolder, 0);
+			viewers[i].setDisplayMode((String) checkBoxs[i].getData());
+			ti.setControl(viewers[i].getControl());
+			ti.setText(IDesignerConst.AE_TYPES[i]);
 		}
+		tabFolder.addSelectionListener(this);
+		widgetSelected(null);
 	}
 
 	@Override
@@ -173,71 +164,21 @@ public class ExtensionWizardPage extends WizardPage implements
 
 	public String[] getUserSelection() {
 		ArrayList<String> als = new ArrayList<String>();
-		for (int i = 0; i < tree.getItemCount(); i++) {
-			TreeItem ti = tree.getItem(i);
-			if (ti.getChecked())
-				als.add((String) ti.getData());
+		for (int i = 0; i < checkBoxs.length; i++) {
+			if (checkBoxs[i].getSelection()) {
+				als.add((String) checkBoxs[i].getData());
+			}
 		}
 		String[] ss = new String[als.size()];
 		als.toArray(ss);
 		return ss;
 	}
 
-	public void mouseEnter(MouseEvent e) {
-	}
-
-	public void mouseExit(MouseEvent e) {
-	}
-
-	public void mouseHover(MouseEvent e) {
-		TreeItem item = tree.getItem(new Point(e.x, e.y));
-		if (item != null) {
-			int idx = tree.indexOf(item);
-			if (tip != null && !tip.isDisposed())
-				tip.dispose();
-			Display display = Display.getDefault();
-			Shell shell = new Shell(display);
-			tip = new Shell(shell, SWT.ON_TOP | SWT.NO_FOCUS | SWT.TOOL);
-			tip.setBackground(display.getSystemColor(SWT.COLOR_INFO_BACKGROUND));
-			FillLayout layout = new FillLayout();
-			layout.marginWidth = 2;
-			tip.setLayout(layout);
-			label = new Label(tip, SWT.NONE);
-			label.setForeground(display
-					.getSystemColor(SWT.COLOR_INFO_FOREGROUND));
-			label.setBackground(display
-					.getSystemColor(SWT.COLOR_INFO_BACKGROUND));
-			label.setText(infos[idx]);
-			Point size = tip.computeSize(SWT.DEFAULT, SWT.DEFAULT);
-			Rectangle rect = item.getBounds(0);
-			Point pt = tree
-					.toDisplay(rect.x + rect.width, rect.y + rect.height);
-			tip.setBounds(pt.x + 10, pt.y, size.x, size.y);
-			tip.setVisible(true);
-		}
-	}
-
-	public void mouseMove(MouseEvent e) {
-		if (tip != null) {
-			tip.setVisible(false);
-			tip.dispose();
-			tip = null;
-			label = null;
-		}
-	}
-
 	public void widgetSelected(SelectionEvent e) {
-		TreeItem[] sels = tree.getSelection();
-		// tree is single selection
-		if (sels.length > 0) {
-			int idx = tree.indexOf(sels[0]);
-			SimpleBMModelViewer v = viewers[idx];
-			layout.topControl = v.getTable();
-			stackComposite.layout();
-			if (v.getInput() == null)
-				v.setInput(model);
-			v.refresh();
-		}
+		SimpleBMModelViewer v = viewers[tabFolder.getSelectionIndex()];
+		if (v.getInput() == null)
+			v.setInput(model);
+		v.refresh();
 	}
 
 	public void widgetDefaultSelected(SelectionEvent e) {
