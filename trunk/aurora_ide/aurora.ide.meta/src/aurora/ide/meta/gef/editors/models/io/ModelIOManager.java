7334cd3ed200f165f0f2c8a0b29e13c4311ff694
jeschou.zsy@gmail.com <jeschou.zsy@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2013-03-22 08:04:35 +0000

    标准化ViewDiagram的IO操作

--- trunk/aurora_ide/aurora.ide.meta/src/aurora/ide/meta/gef/editors/models/io/ModelIOManager.java
+++ trunk/aurora_ide/aurora.ide.meta/src/aurora/ide/meta/gef/editors/models/io/ModelIOManager.java
@@ -10,7 +10,7 @@ import aurora.ide.meta.gef.editors.models.InitProcedure;
 import aurora.ide.meta.gef.editors.models.ModelQuery;
 import aurora.ide.meta.gef.editors.models.ViewDiagram;
 
-public class ModelIOManager {
+public class ModelIOManager extends DefaultIOHandler {
 
 	public static final String NS_CHILDLIST = "cl";
 	public static final String NS_CHILDLIST_URI = "http://meta.editor/childlist";
@@ -29,62 +29,20 @@ public class ModelIOManager {
 	}
 
 	public CompositeMap toCompositeMap(ViewDiagram diagram) {
-		CompositeMap root = new CommentCompositeMap();
-		root.setName(diagram.getClass().getSimpleName());
-		root.put(BIND_TEMPLATE, diagram.getBindTemplate());
-		root.put(TEMPLATE_TYPE, diagram.getTemplateType());
-		// about UnBindModels
-		List<String> list = diagram.getUnBindModels();
-		String str = StringUtil.join(list, ";");
-		root.put(UNBINDMODELS, str);
-		// end UnBindModels
-		InitProcedure ip = diagram.getInitProcedure();
-		if (ip != null) {
-			List<ModelQuery> imList = ip.getModelQuerys();
-			if (imList.size() > 0) {
-				CompositeMap imMap = new CommentCompositeMap(INIT_PROCEDURE);
-				ModelQueryHandler imhandler = new ModelQueryHandler();
-				for (ModelQuery im : imList) {
-					CompositeMap m = imhandler.toCompositeMap(im, mic);
-					imMap.addChild(m);
-				}
-				root.addChild(imMap);
-			}
-		}
-		for (AuroraComponent ac : diagram.getChildren()) {
-			IOHandler ioh = ExtIOHandlerUtil.getHandler(ac);
-			root.addChild(ioh.toCompositeMap(ac, mic));
-		}
+		CompositeMap root = toCompositeMap(diagram, mic);
 		return root;
 	}
 
 	public ViewDiagram fromCompositeMap(CompositeMap root) {
 		if (!ViewDiagram.class.getSimpleName().equalsIgnoreCase(root.getName()))
 			return null;
-		ViewDiagram dia = new ViewDiagram();
-
-		String tpl = root.getString(BIND_TEMPLATE);
-		dia.setBindTemplate(tpl);
-		dia.setTemplateType(root.getString(TEMPLATE_TYPE));
-		// about UnBindModels
-		String str = root.getString(UNBINDMODELS);
-		dia.getUnBindModels().addAll(Arrays.asList(StringUtil.split(str, ";")));
-		// end UnBindModels
-		@SuppressWarnings("unchecked")
-		List<CompositeMap> list = root.getChildsNotNull();
-		for (CompositeMap map : list) {
-			if (map.getName().equals(INIT_PROCEDURE)) {
-				restoreInitProcedure(dia, map);
-				continue;
-			}
-			IOHandler ioh = ExtIOHandlerUtil.getHandler(map);
-			dia.addChild(ioh.fromCompositeMap(map, mic));
-		}
+		ViewDiagram dia = (ViewDiagram) fromCompositeMap(root, mic);
 		recoverReference(dia);
 		return dia;
 	}
 
 	private void restoreInitProcedure(ViewDiagram dia, CompositeMap imMap) {
+		@SuppressWarnings("unchecked")
 		List<CompositeMap> list = imMap.getChildsNotNull();
 		ModelQueryHandler imh = new ModelQueryHandler();
 		for (CompositeMap m : list) {
@@ -98,5 +56,63 @@ public class ModelIOManager {
 			rd.arg = mic.markMap.get(rd.markid);
 			rd.run();
 		}
+	}
+
+	@Override
+	protected void storeSimpleAttribute(CompositeMap map, AuroraComponent ac) {
+		super.storeSimpleAttribute(map, ac);
+		ViewDiagram diagram = (ViewDiagram) ac;
+		map.put(BIND_TEMPLATE, diagram.getBindTemplate());
+		map.put(TEMPLATE_TYPE, diagram.getTemplateType());
+		// about UnBindModels
+		List<String> list = diagram.getUnBindModels();
+		String str = StringUtil.join(list, ";");
+		map.put(UNBINDMODELS, str);
+	}
+
+	@Override
+	protected void storeComplexAttribute(CompositeMap map, AuroraComponent ac) {
+		super.storeComplexAttribute(map, ac);
+		ViewDiagram diagram = (ViewDiagram) ac;
+		InitProcedure ip = diagram.getInitProcedure();
+		if (ip != null) {
+			List<ModelQuery> imList = ip.getModelQuerys();
+			if (imList.size() > 0) {
+				CompositeMap imMap = new CommentCompositeMap(INIT_PROCEDURE);
+				ModelQueryHandler imhandler = new ModelQueryHandler();
+				for (ModelQuery im : imList) {
+					CompositeMap m = imhandler.toCompositeMap(im, mic);
+					imMap.addChild(m);
+				}
+				map.addChild(imMap);
+			}
+		}
+	}
+
+	@Override
+	protected void restoreSimpleAttribute(AuroraComponent ac, CompositeMap map) {
+		super.restoreSimpleAttribute(ac, map);
+		ViewDiagram dia = (ViewDiagram) ac;
+
+		String tpl = map.getString(BIND_TEMPLATE);
+		dia.setBindTemplate(tpl);
+		dia.setTemplateType(map.getString(TEMPLATE_TYPE));
+		// about UnBindModels
+		String str = map.getString(UNBINDMODELS);
+		dia.getUnBindModels().addAll(Arrays.asList(StringUtil.split(str, ";")));
+	}
+
+	@Override
+	protected void restoreComplexAttribute(AuroraComponent ac, CompositeMap map) {
+		super.restoreComplexAttribute(ac, map);
+		CompositeMap initProcedureMap = map.getChild(INIT_PROCEDURE);
+		if (initProcedureMap != null) {
+			restoreInitProcedure((ViewDiagram) ac, initProcedureMap);
+		}
+	}
+
+	@Override
+	protected AuroraComponent getNewObject(CompositeMap map) {
+		return new ViewDiagram();
 	}
 }
