e13f400f884d139e0bc199a7a25abd287de0262f
jeschou.zsy@gmail.com <jeschou@users.noreply.github.com>
2013-10-31 06:11:33 +0000

    直接从buttonclicker获取targetcomponent，从而取得图标

--- aurora.ide.meta/src/aurora/ide/meta/gef/util/ImageUtil.java
+++ aurora.ide.meta/src/aurora/ide/meta/gef/util/ImageUtil.java
@@ -5,13 +5,14 @@ import org.eclipse.swt.graphics.Image;
 import aurora.ide.meta.gef.editors.property.PropertySourceUtil;
 import aurora.plugin.source.gen.screen.model.AuroraComponent;
 import aurora.plugin.source.gen.screen.model.Button;
+import aurora.plugin.source.gen.screen.model.ButtonClicker;
 import aurora.plugin.source.gen.screen.model.IDialogEditableObject;
 
 public class ImageUtil {
 	public static Image getImage(IDialogEditableObject value){
-		Object contextInfo = value.getContextInfo();
-		if(contextInfo instanceof Button){
-			AuroraComponent targetComponent = ((Button) contextInfo).getButtonClicker().getTargetComponent();
+//		Object contextInfo = value.getContextInfo();
+		if(value instanceof ButtonClicker){
+			AuroraComponent targetComponent = ((ButtonClicker)value).getTargetComponent();
 			if(targetComponent!=null){
 				return PropertySourceUtil.getImageOf(targetComponent);
 			}
