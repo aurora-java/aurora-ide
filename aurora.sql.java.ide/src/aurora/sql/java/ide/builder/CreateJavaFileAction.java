721ba491fd12b59e7afc0b77a474605172d7da66
jeschou.zsy@gmail.com <jeschou@users.noreply.github.com>
2014-05-12 05:57:21 +0000

    增加Marker支持

--- aurora.sql.java.ide/src/aurora/sql/java/ide/builder/CreateJavaFileAction.java
+++ aurora.sql.java.ide/src/aurora/sql/java/ide/builder/CreateJavaFileAction.java
@@ -10,23 +10,30 @@ import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.compiler.IProblem;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.ui.ide.undo.CreateFileOperation;
 
 import sqlj.ast.AstTransform;
 import sqlj.core.ParsedSource;
+import sqlj.exception.ParserException;
+import sqlj.exception.TransformException;
 import sqlj.parser.SqljParser;
 
 public class CreateJavaFileAction extends Action {
+	private static String MARKER_TYPE = "org.eclipse.core.resources.problemmarker";
 
-	public void run(IFile sqljFile,IFile javaFile) throws InvocationTargetException,
-			InterruptedException {
+	public void run(IFile sqljFile, IFile javaFile)
+			throws InvocationTargetException, InterruptedException {
 
 		File source = sqljFile.getLocation().toFile();
 		FileInputStream fis;
 		try {
+			sqljFile.deleteMarkers(MARKER_TYPE, true, 0);
 			fis = new FileInputStream(source);
 			byte[] b = new byte[(int) source.length()];
 			fis.read(b);
@@ -38,13 +45,12 @@ public class CreateJavaFileAction extends Action {
 			String str = trans.tranform();
 			InputStream is = new ByteArrayInputStream(str.getBytes("UTF-8"));
 			javaFile.delete(true, null);
-			final CreateFileOperation cfo = new CreateFileOperation(javaFile, null, is,
-					"Create Java File.");
+			final CreateFileOperation cfo = new CreateFileOperation(javaFile,
+					null, is, "Create Java File.");
 			IRunnableWithProgress op = new IRunnableWithProgress() {
 				public void run(IProgressMonitor monitor) {
 					try {
-						cfo.execute(monitor
-								, null);
+						cfo.execute(monitor, null);
 					} catch (ExecutionException e) {
 						e.printStackTrace();
 					}
@@ -55,8 +61,30 @@ public class CreateJavaFileAction extends Action {
 			e2.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (ParserException e) {
+			try {
+				IMarker m = sqljFile.createMarker(MARKER_TYPE);
+				m.setAttribute(IMarker.CHAR_START, e.getStart());
+				m.setAttribute(IMarker.CHAR_END, e.getEnd());
+				m.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
+				m.setAttribute(IMarker.MESSAGE, e.getMessage());
+			} catch (CoreException e1) {
+				e1.printStackTrace();
+			}
+		} catch (TransformException e) {
+			try {
+				for (IProblem p : e.problems) {
+					IMarker m = sqljFile.createMarker(MARKER_TYPE);
+					m.setAttribute(IMarker.CHAR_START, p.getSourceStart());
+					m.setAttribute(IMarker.CHAR_END, p.getSourceEnd() + 1);
+					m.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
+					m.setAttribute(IMarker.MESSAGE, p.getMessage());
+				}
+			} catch (CoreException e1) {
+				e1.printStackTrace();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
 	}
 }
